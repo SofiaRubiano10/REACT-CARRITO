8c62fe7ebffe9ef32fbb1727171c901ebd505fb8
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-06-07 16:26:15 +0200

    Redux Toolkit: Reducer - acción Calculate Totals

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import CartContainer from "./components/cart/CartContainer"
-import NavBar from "./components/navbar/NavBar"
+import NavBar from "./components/navbar/NavBar";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect} from "react";
+import CartContainer from "./components/cart/CartContainer";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 const App = () => {
-
+  // obtiene acceso a los items del cart
+  const { cartItems } = useSelector(store => store.cart)
+  const dispatch = useDispatch();
+  
+  //Recalcula los totales cada vez que camvia cartItems
+  useEffect(() =>{
+    dispatch(calculateTotals());
+    // Recalcula totales
+  }, [cartItems, dispatch])
   return (
     <> 
       <NavBar/>
