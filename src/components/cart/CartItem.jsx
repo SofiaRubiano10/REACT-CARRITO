005c0be273af48720bbd5cc628e63a2fb3a5e5c7
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-06-07 11:54:03 +0200

    Redux Toolkit: SCSS para CartContainer, CartItem y CartFooter

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,14 +1,24 @@
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa'
+
 import PropTypes from 'prop-types'
 
 const CartItem = ({id, title, price, img, amount}) => {
-  console.log(id);
     return (
-        <article>
+        <article key={id} className='cart-item'>
             <img src={img} alt={title} />
             <div>
                 <h3>{title}</h3>
-                <p>${price}</p>
-                <p>{amount}</p>
+                <h3 className='item-price'>${price}</h3>
+                <button className='remove-btn'>Remove</button>
+            </div>
+            <div>
+                <button className='amount-btn'> 
+                    <FaChevronUp/>
+                 </button>
+                <p className='amount'>{amount}</p>
+                <button className='amount-btn'>
+                    <FaChevronDown/>
+                </button>
             </div>
         </article>
   )
